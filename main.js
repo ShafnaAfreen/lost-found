8676cd205479432e30cc7e3f707c2834b3d862d1
ShafnaAfreen <shafnaafreen18@gmail.com>
2025-04-29 12:47:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { UserManager } from "oidc-client-ts";
 const BASE_URL = "https://subtle-jalebi-a0ce07.netlify.app";
 const cognitoAuthConfig = {
-    authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_6B4QWK8Cx",
-    client_id: "7lb9mng5i1vhoh5qr3j2626tv2",
+    authority: "<your authority",
+    client_id: "<your client id>",
     redirect_uri: "https://subtle-jalebi-a0ce07.netlify.app/signin.html",
     response_type: "code",
     scope: "email openid phone"
@@ -12,7 +12,7 @@ const cognitoAuthConfig = {
 export const userManager = new UserManager({ ...cognitoAuthConfig });
 
 export async function signOutRedirect() {
-    const clientId = "7lb9mng5i1vhoh5qr3j2626tv2";
+    const clientId = "<client id>";
     const logoutUri = "https://subtle-jalebi-a0ce07.netlify.app/signout.html";
     const cognitoDomain = "https://eu-north-16b4qwk8cx.auth.eu-north-1.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
